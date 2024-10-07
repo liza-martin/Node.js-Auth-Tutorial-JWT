f332295460b3b4859e243b0dd072cc631d1ce9b9
liza-martin <lmartin272@ivytech.edu>
2024-10-07 17:18:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes.js');
 const cookieParser = require('cookie-parser');
-const { requireAuth, checkUser } = require('./middleware/authMiddleware');
+const { requireAuth, checkUser } = require('./middleware/authMiddleware.js');
 
 const app = express();
 
